d63b2c7bb4866a548252b540c8d098c6d0c344eb
neelanjan00 <neelanjan@chaosnative.com>
2021-09-17 20:05:55 +0530

    host-operations.go and host-status.go added
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

---
+++ pkg/cloud/vmware/host-operations.go
@@ -0,0 +1,37 @@
+package vmware
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+
+	"github.com/pkg/errors"
+)
+
+// shellout executes a given shell command and returns the stdout, stderr, and execution error
+func shellout(command string) (string, string, error) {
+	var stdout, stderr bytes.Buffer
+
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+
+	return stdout.String(), stderr.String(), err
+}
+
+// RebootHost causes a given ESX host to reboot using the host URL and datacenter name
+func RebootHost(hostURL, datacenter string) error {
+
+	cmd := fmt.Sprintf("govc host.shutdown -r=true -f=true -dc=%s %s", datacenter, hostURL)
+	_, stderr, err := shellout(cmd)
+
+	if err != nil {
+		return err
+	} else if stderr != "" {
+		return errors.Errorf("%s", stderr)
+	}
+
+	return nil
+}
---
+++ pkg/cloud/vmware/host-status.go
@@ -0,0 +1,115 @@
+package vmware
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+type ErrorResponse struct {
+	MsgValue struct {
+		MsgMessages []struct {
+			MsgDefaultMessage string `json:"default_message"`
+		} `json:"messages"`
+	} `json:"value"`
+}
+
+// GetHostDetails returns the host id, host connection state, and host power state for a given host URL
+func GetHostDetails(vcenterServer, hostURL, cookie string) (string, string, string, error) {
+
+	type Host struct {
+		MsgValue []struct {
+			MsgHost            string `json:"host"`
+			MsgConnectionState string `json:"connection_state"`
+			MsgPowerState      string `json:"power_state"`
+		} `json:"value"`
+	}
+
+	req, err := http.NewRequest("GET", "https://"+vcenterServer+"/rest/vcenter/host?filter.names.1="+hostURL, nil)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", cookie)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", "", "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		var errorResponse ErrorResponse
+
+		json.Unmarshal(body, &errorResponse)
+
+		return "", "", "", errors.Errorf("error during the fetching of host details: %s", errorResponse.MsgValue.MsgMessages[0].MsgDefaultMessage)
+	}
+
+	var hostDetails Host
+	json.Unmarshal(body, &hostDetails)
+
+	return hostDetails.MsgValue[0].MsgHost, hostDetails.MsgValue[0].MsgConnectionState, hostDetails.MsgValue[0].MsgPowerState, nil
+}
+
+// GetPoweredOnVMDetails returns the details of the VMs that are in POWERED_ON and are attached to a given host
+func GetPoweredOnVMDetails(vcenterServer, host, cookie string) (string, error) {
+
+	type VM struct {
+		MsgValue []struct {
+			MsgVm string `json:"vm"`
+		} `json:"value"`
+	}
+
+	req, err := http.NewRequest("GET", "https://"+vcenterServer+"/rest/vcenter/vm?filter.hosts.1="+host+"&filter.power_states.1=POWERED_ON", nil)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", cookie)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		var errorResponse ErrorResponse
+
+		json.Unmarshal(body, &errorResponse)
+
+		return "", errors.Errorf("error during the fetching of VM details: %s", errorResponse.MsgValue.MsgMessages[0].MsgDefaultMessage)
+	}
+
+	var vmDetails VM
+	json.Unmarshal(body, &vmDetails)
+
+	return vmDetails.MsgValue[0].MsgVm, nil
+}
